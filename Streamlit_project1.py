cf1041cec948ef9fefa28b6126b9569a5df8ff02
EllieDam <97864757+EllieDam@users.noreply.github.com>
2022-03-25 10:54:43 +0700

    Add files via upload

--- Streamlit_project1.py
+++ Streamlit_project1.py
@@ -16,9 +16,10 @@ warnings.filterwarnings('ignore')
 
 from sklearn import metrics
 import import_ipynb
-# import pickle
+import pickle
 import streamlit as st
 from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import MinMaxScaler
 import Lib
 import seaborn as sns 
 
@@ -29,12 +30,6 @@ df = pd.read_csv("OnlineRetail.zip", encoding='unicode_escape')
 # GUI
 st.title("Data Science Projects")
 st.header("PROJECT 1 - Customer Segmentation")
-
-# # Upload file
-# uploaded_file = st.file_uploader('Choose a file', type=['csv'])
-# if uploaded_file is not None:
-#     data = pd.read_csv(uploaded_file, encoding='latin-1')
-#     data.to_csv('OnlineRetail_new.csv', index=False)
 
 # 2. Data pre-processing
 # drop rows where Quantity < 0
@@ -65,40 +60,48 @@ df_RFM = df.groupby('CustomerID').agg({'InvoiceDate' : Recency,
                                         })
 # Rename column names
 df_RFM.columns = ['Recency', 'Frequency', 'Monetary']
+Monetary_mean = df_RFM.Monetary.mean()
 
-# Create labels for Recency, Frequency, Monetary & Assign them to 5 equal percentile groups and convert their labels from categorical to integer
-r_groups = pd.qcut(df_RFM['Recency'].rank(method='first'), q=5, labels=range(5, 0, -1)).astype(int)
-f_groups = pd.qcut(df_RFM['Frequency'].rank(method='first'), q=5, labels=range(1,6)).astype(int)
-m_groups = pd.qcut(df_RFM['Monetary'].rank(method='first'), q=5, labels=range(1,6)).astype(int)
+# Create labels for Recency, Frequency, Monetary & Assign them to 4 equal percentile groups and convert their labels from categorical to integer
+r_groups = pd.qcut(df_RFM['Recency'].rank(method='first'), q=4, labels=range(4, 0, -1)).astype(int)
+f_groups = pd.qcut(df_RFM['Frequency'].rank(method='first'), q=4, labels=range(1,5)).astype(int)
+m_groups = pd.qcut(df_RFM['Monetary'].rank(method='first'), q=4, labels=range(1,5)).astype(int)
 
 # Create new columns R, F, M
 df_RFM = df_RFM.assign(R=r_groups, F=f_groups, M=m_groups)
 
-# 3. Build model
-gmm = GaussianMixture(n_components=5, covariance_type='spherical', random_state=0)
-gmm.fit(df_RFM[['Recency','Frequency','Monetary']])
+# Remove outliers
+data_RFM_no, data_outlier, max_m, max_f, max_r = Lib.drop_outliers(df_RFM)
+elite, regular, ghost = Lib.elite_regular_ghost_group(df_RFM, data_outlier, max_m, max_f, max_r, Monetary_mean)
+elite["Cluster"] = 4
+ghost["Cluster"] = 1
+regular["Cluster"] = 3
 
-gmm_segment = gmm.predict(df_RFM[['Recency','Frequency','Monetary']])
-df_RFM['GMM_segment'] = gmm_segment
+# Scale data
+scaler = MinMaxScaler()
+data_RFM_scaled = data_RFM_no.copy()
+scaler = scaler.fit(data_RFM_scaled[['Recency','Frequency','Monetary']])
+data_RFM_scaled[['Recency','Frequency','Monetary']]= scaler.transform(data_RFM_scaled[['Recency','Frequency','Monetary']])
 
-# Calculate average values for each GMM_segment, and return a size of each segment 
-gmm_agg = df_RFM.groupby('GMM_segment').agg({
-    'Recency': 'mean',
-    'Frequency': 'mean',
-    'Monetary': 'mean',
-    'R': 'mean',
-    'F': 'mean',
-    'M': ['mean', 'count']}).round(0)
+# 3. Load model
+with open('CS_KMean_Scaled_No.pkl', 'rb') as file:  
+    kmeans_model = pickle.load(file)
 
-gmm_agg.columns = gmm_agg.columns.droplevel()
-gmm_agg.columns = ['RecencyMean','FrequencyMean','MonetaryMean', 'RMean','FMean','MMean','Count']
-gmm_agg['Percent'] = round((gmm_agg['Count']/gmm_agg.Count.sum())*100, 2)
+data_RFM_kmeans = data_RFM_no.copy()
+data_RFM_kmeans['Cluster'] = kmeans_model.labels_
+data_RFM_kmeans2 = data_RFM_kmeans.copy()
+data_RFM_kmeans = data_RFM_kmeans.append(elite)
+data_RFM_kmeans = data_RFM_kmeans.append(ghost)
+data_RFM_kmeans = data_RFM_kmeans.append(regular)
+
+# Calculate average values for each Cluster, and return a size of each segment 
+km_agg = Lib.create_df_agg(data_RFM_kmeans, 'Cluster')
 
 # Reset the index
-gmm_agg = gmm_agg.reset_index()
-dict_seg = {0:'Member', 1:'VIP1', 2:'VIP2', 4:'SVIP', 3:'Loyal/Regular'}
-gmm_agg['GMM_segment_name'] = gmm_agg['GMM_segment'].apply(lambda x: dict_seg[x])
-df_RFM['GMM_segment_name'] = df_RFM['GMM_segment'].apply(lambda x: dict_seg[x])
+km_agg = km_agg.reset_index(drop=True)
+dict_seg = {0:'Gold', 1:'Ghost', 2:'Member', 3:'Deluxe', 4:'Platinum'}
+km_agg['Segment_name'] = km_agg['Cluster'].apply(lambda x: dict_seg[x])
+data_RFM_kmeans['Segment_name'] = data_RFM_kmeans['Cluster'].apply(lambda x: dict_seg[x])
 
 # #4. Save models
 # pkl_filename = "Customer_Segmentation_GMM.pkl"  
@@ -112,6 +115,9 @@ df_RFM['GMM_segment_name'] = df_RFM['GMM_segment'].apply(lambda x: dict_seg[x])
 # GUI
 menu = ['Business Objective', 'Build Project', 'New Prediction']
 choice = st.sidebar.selectbox('Menu', menu)
+st.sidebar.image('img1.png')
+st.sidebar.image('img3.png')
+# st.sidebar.image('img2.png', caption='Customer Segmentation')
 if choice == 'Business Objective':
     st.subheader('Business Objective')
     st.write("""
@@ -162,7 +168,7 @@ elif choice == 'Build Project':
 
     with col1:
         fig = plt.figure(figsize=(8, 5))
-        count = df_RFM.groupby(df_RFM['GMM_segment_name']).size()
+        count = data_RFM_kmeans.groupby(data_RFM_kmeans['Segment_name']).size()
         fig, ax =  plt.subplots()
         ax.bar(count.index, count.values, color='lightskyblue')
         for container in ax.containers:
@@ -170,44 +176,56 @@ elif choice == 'Build Project':
         plt.xticks(rotation=90)
         st.pyplot(fig)
 
-    st.dataframe(gmm_agg)
+    st.dataframe(km_agg.iloc[:,[0,9,1,2,3,4,5,6,7,8]])
     st.write("""
     Cluster description:
-    - Cluster 4 - SVIP: Nhóm khách hàng đặc biệt (chỉ có 2 người) chi tiêu rất khủng, mua hàng thường xuyên nhất và vẫn đang mua hàng.
-    - Cluster 1 - VIP1: Nhóm khách hàng chi tiêu cao, mua hàng thường xuyên và mới mua hàng gần đây.
-    - Cluster 2 - VIP2: Nhóm khách hàng chi tiêu cao nhưng ít hơn VIP1, mua hàng thường xuyên và mới mua hàng gần đây.
-    - Cluster 3 - Loyal/Regular: Khách hàng mua hàng đều đặn, chi tiêu ở mức khá, thời gian mua hàng gần nhất không xa.
-    - Cluster 0 - Member: Khách hàng mua hàng không thường xuyên, chi tiêu không cao và đã khá lâu không mua hàng.
+    - Cluster 4 - PLATINUM    : Huge spending or Frequent purchase and High spending
+    - Cluster 0 - GOLD        : High spending, frequent purchase and recently buying
+    - Cluster 3 - DELUXE      : Medium-high spending, , quite frequent purchase and recently buying
+    - Cluster 2 - MEMBER      : Low spending, infrequent purchase and have not purchased for a long time
+    - Cluster 1 - GHOST       : Only few times purchase, low spending and have not purchased for a long time
     """)  
-    st.dataframe(df_RFM.head())
+    st.write('RESULTS')
+    st.dataframe(data_RFM_kmeans.head())
     fig = plt.figure(figsize=(15, 6))
     sns.set_style('darkgrid')
     plt.subplot(1,3,1)
-    sns.boxplot(x='GMM_segment_name', y='Recency', data=df_RFM)
+    sns.boxplot(x='Segment_name', y='Recency', data=data_RFM_kmeans)
     plt.title('Recency')
     plt.subplot(1,3,2)
-    sns.boxplot(x='GMM_segment_name', y='Frequency', data=df_RFM)
+    sns.boxplot(x='Segment_name', y='Frequency', data=data_RFM_kmeans)
     plt.title('Frequency')
     plt.subplot(1,3,3)
-    sns.boxplot(x='GMM_segment_name', y='Monetary', data=df_RFM)
+    sns.boxplot(x='Segment_name', y='Monetary', data=data_RFM_kmeans)
     plt.title('Monetary')
     plt.tight_layout()
     st.pyplot(fig)
 
     # Visualize results
     fig = plt.figure(figsize=(10, 5))
-    Lib.treemap_customer_segmentation(gmm_agg,font_size=14)
+    # km_agg2 = km_agg.iloc[:,[0,1,2,3,7,8]]
+    Lib.treemap_customer_segmentation(km_agg.iloc[:,[0,1,2,3,7,8]],font_size=14)
     st.pyplot(fig)
 
     # Visualization - 2D Scatter
-    fig = px.scatter(gmm_agg, x="RecencyMean", y="MonetaryMean", size="FrequencyMean", color="GMM_segment",
-           hover_name="GMM_segment", size_max=80)
+    fig = px.scatter(km_agg, x="RecencyMean", y="MonetaryMean", size="FrequencyMean", color="Cluster",
+           hover_name="Cluster", size_max=80)
     st.plotly_chart(fig)
 
     # Visualization - 3D scatter
-    fig = px.scatter_3d(df_RFM, x='Recency', y='Frequency', z='Monetary',
-                        color = 'GMM_segment', opacity=0.5)
+    data_RFM_kmeans2['Monetary'] = np.log(data_RFM_kmeans2['Monetary'])
+    data_RFM_kmeans2['Frequency'] = np.log(data_RFM_kmeans2['Frequency'])
+    data_RFM_kmeans2['Recency'] = np.log(data_RFM_kmeans2['Recency'])
+    # data_RFM_kmeans2.head()
+    fig = px.scatter_3d(data_RFM_kmeans2, x='Recency', y='Frequency', z='Monetary',
+                        color = 'Cluster', opacity=0.5)
     fig.update_traces(marker=dict(size=5),selector=dict(mode='markers'))
+    fig.add_trace(go.Scatter3d(x=np.log(ghost['Recency']), y=np.log(ghost['Frequency']), z=np.log(ghost['Monetary']), 
+                          mode='markers',marker=dict(color='green', symbol='cross'), textposition='top left', showlegend=False))
+    fig.add_trace(go.Scatter3d(x=np.log(elite['Recency']), y=np.log(elite['Frequency']), z=np.log(elite['Monetary']), 
+                          mode='markers',marker=dict(color='red', symbol='cross'), textposition='top left', showlegend=False))
+    fig.add_trace(go.Scatter3d(x=np.log(regular['Recency']), y=np.log(regular['Frequency']), z=np.log(regular['Monetary']), 
+                          mode='markers',marker=dict(color='blue', symbol='cross'), textposition='top left', showlegend=False))
     st.plotly_chart(fig)
 
 elif choice == 'New Prediction':
@@ -223,23 +241,56 @@ elif choice == 'New Prediction':
             flag=True
 
         if flag:
-            st.write('Result:')
+            st.write('Result (saved to Results.csv):')
             if data.shape[0]>0:
                 #Preprocessing 
                 data_RFM = Lib.df_RMF_preprocessing(data)
-                gmm_segment = gmm.predict(data_RFM[['Recency','Frequency','Monetary']])
-                data_RFM['GMM_segment'] = gmm_segment
-                data_RFM['GMM_segment_name'] = data_RFM['GMM_segment'].apply(lambda x: dict_seg[x])
-                data_RFM.to_csv('Results.csv')
-                st.dataframe(data_RFM)
-                value_counts = data_RFM['GMM_segment_name'].value_counts()
+                data_RFM_no, data_outlier = Lib.drop_outliers_predict(data_RFM, max_m, max_f, max_r)
+                elite2, regular2, ghost2 = Lib.elite_regular_ghost_group(data_RFM, data_outlier, max_m, max_f, max_r, Monetary_mean)
+                data_RFM_scaled = scaler.transform(data_RFM_no[['Recency','Frequency','Monetary']])
+                segment = kmeans_model.predict(data_RFM_scaled)
+                data_RFM_no['Cluster'] = segment
+                if elite2.shape[0]>0:
+                    elite2["Cluster"] = 4
+                    data_RFM_no = data_RFM_no.append(elite2)
+                if ghost2.shape[0]>0:
+                    ghost2["Cluster"] = 1
+                    data_RFM_no = data_RFM_no.append(ghost2)
+                if regular2.shape[0]>0:
+                    regular["Cluster"] = 3
+                    data_RFM_no = data_RFM_no.append(regular2)
+                data_RFM_no['Segment_name'] = data_RFM_no['Cluster'].apply(lambda x: dict_seg[x])
+                data_RFM_no = data_RFM_no.sort_values('CustomerID', ascending=True)
+                data_RFM_no.to_csv('Results.csv')
+                
+                st.dataframe(data_RFM_no)
+                value_counts = data_RFM_no['Segment_name'].value_counts()
                 st.code(value_counts)
-                fig = px.scatter_3d(df_RFM, x='Recency', y='Frequency', z='Monetary',
-                    color = 'GMM_segment', opacity=0.5)
+                
+                #Visualization
+                # data_RFM_kmeans2['Monetary'] = np.log(data_RFM_kmeans2['Monetary'])
+                # data_RFM_kmeans2['Frequency'] = np.log(data_RFM_kmeans2['Frequency'])
+                # data_RFM_kmeans2['Recency'] = np.log(data_RFM_kmeans2['Recency'])
+                # fig = px.scatter_3d(data_RFM_kmeans2, x='Recency', y='Frequency', z='Monetary',
+                #                     color = 'Cluster', opacity=0.5)
+                # fig.update_traces(marker=dict(size=5),selector=dict(mode='markers'))
+                # fig.add_trace(go.Scatter3d(x=np.log(ghost['Recency']), y=np.log(ghost['Frequency']), z=np.log(ghost['Monetary']), 
+                #                     mode='markers',marker=dict(color='green', symbol='cross'), textposition='top left', showlegend=False))
+                # fig.add_trace(go.Scatter3d(x=np.log(elite['Recency']), y=np.log(elite['Frequency']), z=np.log(elite['Monetary']), 
+                #                     mode='markers',marker=dict(color='red', symbol='cross'), textposition='top left', showlegend=False))
+                # fig.add_trace(go.Scatter3d(x=np.log(regular['Recency']), y=np.log(regular['Frequency']), z=np.log(regular['Monetary']), 
+                #                     mode='markers',marker=dict(color='blue', symbol='cross'), textposition='top left', showlegend=False))
+                
+                data_RFM_kmeans['Monetary'] = np.log(data_RFM_kmeans['Monetary'])
+                data_RFM_kmeans['Frequency'] = np.log(data_RFM_kmeans['Frequency'])
+                data_RFM_kmeans['Recency'] = np.log(data_RFM_kmeans['Recency'])                
+                fig = px.scatter_3d(data_RFM_kmeans, x='Recency', y='Frequency', z='Monetary',
+                                    color = 'Cluster', opacity=0.5)
                 fig.update_traces(marker=dict(size=5),selector=dict(mode='markers'))
-
-                fig.add_trace(go.Scatter3d(x=data_RFM['Recency'], y=data_RFM['Frequency'], z=data_RFM['Monetary'], 
-                           mode='markers',marker=dict(color='green', symbol='cross'), textposition='top left', showlegend=False))
+                
+                fig.add_trace(go.Scatter3d(x=np.log(data_RFM_no['Recency']), y=np.log(data_RFM_no['Frequency']), z=np.log(data_RFM_no['Monetary']), 
+                           mode='markers',marker=dict(color='cyan', size=6,line=dict(color='black',width=40), symbol='x'), 
+                           textposition='top left', showlegend=False))
                 st.plotly_chart(fig)
     else:
         col1, col2, col3 = st.columns(3)
@@ -252,12 +303,44 @@ elif choice == 'New Prediction':
         with col3:
             monetary = st.number_input('Monetary', min_value=0.00,)
             st.write('The current number is ', monetary)
-        if (recency!=0) & (frequency!=0) & (monetary!=0):
-            gmm_segment2 = gmm.predict([[recency,frequency,monetary]])
-            st.write('Customer Segment:', dict_seg[gmm_segment2[0]])
-            fig = px.scatter_3d(df_RFM, x='Recency', y='Frequency', z='Monetary',
-                        color = 'GMM_segment', opacity=0.5)
+        if (recency>=0) & (frequency>0) & (monetary>0):
+            df_rfm = pd.DataFrame([recency,frequency,monetary]).T
+            df_rfm.columns = ['Recency','Frequency','Monetary']            
+            df_rfm_no, data_outlier = Lib.drop_outliers_predict(df_rfm, max_m, max_f, max_r)
+            if df_rfm_no.shape[0]>0:
+                df_rfm_no = scaler.transform(df_rfm_no)
+            
+            elite, regular, ghost = Lib.elite_regular_ghost_group(df_rfm, data_outlier, max_m, max_f, max_r, Monetary_mean)
+            if elite.shape[0]>0:
+                    cluster = 4
+                    result = 'Platinum'
+            elif ghost.shape[0]>0:
+                    cluster = 1
+                    result = 'Ghost'
+            elif regular.shape[0]>0:
+                    cluster = 3
+                    result = 'Deluxe'
+            else:
+                segment2 = kmeans_model.predict(df_rfm_no)
+                cluster = segment2[0]
+                result = dict_seg[cluster]
+            st.write('Customer Segment:', cluster, ' - ',result)
+            #Visualization 
+            data_RFM_kmeans['Monetary'] = np.log(data_RFM_kmeans['Monetary'])
+            data_RFM_kmeans['Frequency'] = np.log(data_RFM_kmeans['Frequency'])
+            data_RFM_kmeans['Recency'] = np.log(data_RFM_kmeans['Recency'])                
+            fig = px.scatter_3d(data_RFM_kmeans, x='Recency', y='Frequency', z='Monetary',
+                                    color = 'Cluster', opacity=0.5)
             fig.update_traces(marker=dict(size=5),selector=dict(mode='markers'))
-            fig.add_trace(go.Scatter3d(x=[recency], y=[frequency], z=[monetary], 
-                            mode='markers',marker=dict(color='green', symbol='cross',size=15), textposition='top left', showlegend=False))
+            
+            # fig = px.scatter_3d(data_RFM_kmeans2, x='Recency', y='Frequency', z='Monetary',
+            #             color = 'Cluster', opacity=0.5)
+            # fig.update_traces(marker=dict(size=5),selector=dict(mode='markers'))
+            fig.add_trace(go.Scatter3d(x=np.log([recency]), y=np.log([frequency]), z=np.log([monetary]), 
+                            mode='markers',marker=dict(color='cyan', line=dict(color='black',width=40), symbol='x',size=10), 
+                            textposition='top left', showlegend=False))
             st.plotly_chart(fig)
+        else: 
+            st.code(
+            """ValueError: \n- Valid values: Recency>=0, Frequency>0, Monetary>0""")
+            
